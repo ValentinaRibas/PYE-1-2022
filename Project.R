079b360de2d086473aca56bb1f64a0da03a61de5
astorlucas <lucasastor@gmail.com>
2022-10-04 14:26:23 -0300

    ejercicio 1 actualizado

--- Project.R
+++ Project.R
@@ -2,16 +2,16 @@
 # distribución binomial (30 ,  0.15)
 # n = 10 ^ (2 al 5)
 # rbinom(
-#       n,     NÃºmero de observaciones aleatorias a ser generadas
-#       size,  NÃºmero de ensayos (> = 0)
-#       prob)  La probabilidad de éito en cada ensayo
+#       n,     Numero de observaciones aleatorias a ser generadas
+#       size,  Numero de ensayos (> = 0)
+#       prob)  La probabilidad de éxito en cada ensayo
 
 # Generamos 4 constantes "a", "b", "c" y "d" aplicando la distribución binomial
 # para los tamaños de muestra 10^2, 10^3, 10^4 y 10^5 respectivamente
-a = rbinom(10^2,30, 0.15)
-b = rbinom(10^3,30, 0.15)
-c = rbinom(10^4,30, 0.15)
-d = rbinom(10^5,30, 0.16)
+a = rpois(10^2,lambda = 3.5)
+b = rpois(10^3,lambda = 3.5)
+c = rpois(10^4,lambda = 3.5)
+d = rpois(10^5,lambda = 3.5)
 
 # Para graficar las 4 mustras en un mismo gráfico de cajas generamos un array
 # con los resultados
@@ -19,21 +19,21 @@ d = rbinom(10^5,30, 0.16)
 
 # agregamos un data.frame para identificar cada grÃ¡fico
 #data <- data.frame(values =randomValues,
-                   #group = c("10^2","10^3","10^4","10^5"))
+#group = c("10^2","10^3","10^4","10^5"))
 
 # Con boxplot generamos el gráfico de cajas agregado color para distiguir 
 # cada gráfico y borramos el eje "y" para agregar uno más exacto
 boxplot(#values ~ group,
-        #data,
-        a,b,c,d,
-        col = c("pink",
-                "beige", 
-                "lightblue", 
-                "lightgreen"),
-        main = "Distribución binomial",
-        xlab = "Muestras",
-        ylab = "Valor variable aleatoria",
-        yaxt='n')
+  #data,
+  a,b,c,d,
+  col = c("pink",
+          "beige", 
+          "lightblue", 
+          "lightgreen"),
+  main = "Distribución binomial",
+  xlab = "Muestras",
+  ylab = "Valor variable aleatoria",
+  yaxt='n')
 
 #Mostramos los datos particulares de cada una de las muestras obtenidas.
 boxplot.stats(a)
@@ -42,7 +42,7 @@ boxplot.stats(c)
 boxplot.stats(d)
 
 
-# Agregamos eje "y" mÃ¡s exacto
+# Agregamos eje "y" mas exacto
 axis(2, at=seq(0, 30, 1),las=2)
 
 
@@ -53,53 +53,65 @@ espB = mean(b)
 espC = mean(c)
 espD = mean(d)
 
-# Utilizamos el producto número de ensayos y la probabilidad de éxito para 
-# hallar la esperanza teórica
-espT = 30*0.15
+# Por el teórico se sabe que lambda coincide con la esperanza teórica
+espT = 3.5
 
-# Utilizamos "var" para calcular la varainza empírica de cada muestra
+# Se compara el valor teórico contra el valor empirico obtenido en cada caso
+comparEA = espA - espT
+comparEB = espB - espT
+comparEC = espC - espT
+comparED = espD - espT
+# Se observa que en efecto cuando se aumenta la cantidad de ensayos el resultado se parece más al teorico
+
+# Utilizamos el comando "var" para calcular la varainza empírica de cada muestra
 # y guardamos cada resultado para su posterior comparación
 varA = var(a)
 varB = var(b)
 varC = var(c)
 varD = var(d)
 
-# Utilizamos el producto del numero de ensayos, la probabilidad de Ã©xito y 
-# la probabilidad de fracaso para obtener la varianza teórica
-varT = 30*0.15*0.85
+# Por el teórico se sabe que lambda coincide con el valor de la varianza
+varT = 3.5
 
-# Con "ecdf" calculamos la distribuciÃ³n empírica para las muestras
-# de tamaÃ±o 10^2 y 10^5
+# Se compara el valor teorico de la varianza contra el valor empirico obtenido en cada caso
+comparVA = varA - varT
+comparVB = varB - varT
+comparVC = varC - varT
+comparVD = varD - varT
+# Se observa que en efecto cuando se aumenta la cantidad de ensayos el resultado se parece más al teorico
+
+# Con "ecdf" calculamos la distribución empírica para las muestras
+# de tamaño 10^2 y 10^5
 distA = ecdf(a)
 distD = ecdf(d)
 
-# Utilizamos "stepfun" con una lista de 1 a 10 junto a pbinom para obetner
-# obtener la funciÃ³n de distribuiÃ³n acumulada teÃ³rica y poder graficarla
-# utilizando "plot". genrando los escalones caracterÃ­sticos. Nos aseguramos
+# Utilizamos "stepfun" con una lista de 1 a 10 junto a ppois para obetner
+# obtener la funcion de distribucion acumulada teorica y poder graficarla
+# utilizando "plot". genrando los escalones caracteristicos. Nos aseguramos
 # de eliminar las verticales y de agregar las etiquetas correspondientes
-plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)), 
+plot(stepfun(c(1:10),ppois(c(0,1:10), lambda = 3.5)), 
      main = "Función de distribución acumulada para 10^2", 
      xlab = "k",
      ylab = "F(k)",
      col="red")
 
 
-# Sobre el grÃ¡fico anterior graficamos nuestra desitribuciÃ³n empirica 
-# con tamaÃ±o de muestera 10^2 que obtuubimos previmante con un color
-# diferente para distinguirla de la distribuciÃ³n teÃ³rica
+# Sobre el grafico anterior graficamos nuestra desitribucion empirica 
+# con tamaño de muestera 10^2 que obtuvimos previmante con un color
+# diferente para distinguirla de la distribucion teorica
 lines(distA, col="springgreen4")
 
-# Agregamos una leyenda para hacer mÃ¡s clara la identificaciÃ³n
-# de los grÃ¡ficos
+# Agregamos una leyenda para hacer mas clara la identificacion
+# de los graficos
 legend("bottomright", 
        legend = c("Distribución Acumulada Empírica - 10^2",
                   "Distribución Acumulada Teórica"),
        lty = 1, col = c("red","springgreen4"), lwd = 2, box.lty =1)
 
 
-# Analogamente al grÃ¡fico anterior grÃ¡ficamos la distribuciÃ³n acumulada teÃ³rica
+# Analogamente al grafico anterior graficamos la distribucion acumulada teorica
 
-plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)), 
+plot(stepfun(c(1:10),ppois(c(0,1:10), lambda = 3.5)), 
      main = "Función de distribución acumulada para 10^5", 
      xlab = "k",
      ylab = "F(k)",
@@ -129,22 +141,22 @@ d = rnorm(10^5,-4, sqrt(16))
 
 #randomValues <- c(a,b,c,d)
 #data <- data.frame(values =randomValues,
-                   #group = c("10^2","10^3","10^4","10^5"))
+#group = c("10^2","10^3","10^4","10^5"))
 
 # Con boxplot generamos el grÃ¡fico de cajas agregado color para distiguir 
 # cada grÃ¡fico y borramos el eje "y" para agregar uno mÃ¡s exacto, al igual
 # que lo hicimos con la distribuciÃ³n binomial
 boxplot(#values ~ group,
-        #data,
-        a,b,c,d,
-        col = c("pink",
-                "beige", 
-                "lightblue", 
-                "lightgreen"),
-        main = "Distribución normal",
-        xlab = "Muestras",
-        ylab = "Valor variable aleatoria", 
-        yaxt='n')
+  #data,
+  a,b,c,d,
+  col = c("pink",
+          "beige", 
+          "lightblue", 
+          "lightgreen"),
+  main = "Distribución normal",
+  xlab = "Muestras",
+  ylab = "Valor variable aleatoria", 
+  yaxt='n')
 
 # Agregamos eje "y" mÃ¡s exacto
 axis(2, at=seq(-20, 20, 1),las=2)
@@ -248,9 +260,9 @@ medEsta = sqrt(1000) * ((muEmp - muTeo)/desvTeo)
 # Para la parte 2 realizamos un loop for para realizar los calculos de la parte "A" y guardarlas en un array 
 promEst <- 0
 for(i in 1:500){
-        ab = rnorm(10^3,-4, sqrt(16))
-        muEmpb = mean(ab) 
-        promEst[i] <- sqrt(10^3) * ((muEmpb - muTeo)/desvTeo)
+  ab = rnorm(10^3,-4, sqrt(16))
+  muEmpb = mean(ab) 
+  promEst[i] <- sqrt(10^3) * ((muEmpb - muTeo)/desvTeo)
 }
 
 # Presentamos en un histograma los valores obtenidos en el loop
